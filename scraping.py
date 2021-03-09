35cbde431b082f2911e91f8b700e03008e2193aa
Dirk Ulbricht <dirk.ulbricht@gmail.com>
2021-03-09 18:21:04 +0100

    mieten, kaufen, wohnung geprüft

--- scraping.py
+++ scraping.py
@@ -1,17 +1,10 @@
 import urllib3
 from lxml import etree
 import re
-# Erledigt: lxml.tree und response verwendet, um die Seiten zu scapen.
-# Die wichtigsten Eigenschaften können gelesen werden
-# To do:
-# 1. Die Eigenschaften müssen Art-übergreifend (miete, kaufen, haus, wohnung)
-#  unabhängig mit try-except Konstrukten gelesen werden.
-# 2. Das scraping muss in main integriert werden.
-# 3. Das scraping muss bereinigt werden ("\nr     ", bspw. muss raus, nach einem ":"
-# muss eine Ausprägung als solche erkannt werden.)
+
 test = '2021-02-28-hamburg-haeuser-kaufen.txt'
-test = '2021-02-28-hamburg-wohnungen-kaufen.txt'
-test = '2021-02-28-ludwigslust-meckl-haus-mieten.txt'
+#test = '2021-02-28-hamburg-wohnungen-kaufen.txt'
+#test = '2021-02-28-ludwigslust-meckl-haus-mieten.txt'
 
 def prepare_urls(filename):
     """read text_file containing expose-ids, return list of urls"""
@@ -28,54 +21,19 @@ def get_id_from_url(url):
     id = re.search(r'(?<=expose\/).*',url)
     return id.group(0)
 
-#--------------veraltete Scraping Version, Fehler: hat kein Try/Except für alle Felder einzeln,
-# hat noch kein Data Cleaning.
-# hat noch keine beschreibung und Energie mit aufgenommen.
-def scrape_houses(urls):
-    data = {}
-    request = urllib3.PoolManager()
-
-    for url in urls:
-
-        res = request.request("GET", url)
-        parser = etree.HTMLParser(recover=True)
-        tree = etree.HTML(res.data, parser)
-        id = get_id_from_url(url)
-
-        try:
-            data[id] ={
-                'url':url,
-                'title': tree.xpath('//title/text()')[0],
-                'ort': tree.xpath('//div[@class="location"]/span/text()'),
-                'merkmale':  tree.xpath('//div[@class="merkmale"]/text()'),
-                'stadteilbewertung':  tree.xpath('//div[contains(@id,"divRating")]'),
-                'kaufpreis': tree.xpath('//div[@class="hardfact"]/strong/strong/text()'),
-                'anzahl_raeume': tree.xpath('//div[@class="hardfact rooms"]/text()'),
-                'wohnflaeche': tree.xpath('//div[@class="hardfact "]/text()')[3],
-                'grundstuecksflaeche': tree.xpath('//div[@class="hardfact "]/text()')[5],
-                #'energielabel': tree.xpath('//div[@class="datatable energytable clear"]/div/span[@class="datalabel"]/text()')[0],
-                #'energiemerkmale': tree.xpath('//div[@class="datatable energytable clear"]/div/span[@class="datacontent"]/text()')[0],
-                #'energiemerkmale_checkbox':tree.xpath('//ul[contains(string(),"Haustyp")]//text()'),
-                #'objektbeschreibung':tree.xpath('//div[contains(string(),"Objektbeschreibung")]//p/text()'),
-                }
-        except:
-            print("id ",id," could not be retrieved. Most likely, it has been taken offline.")
-    return data
-
-data = scrape_houses(urls)
-data
 
 #----------------------Kompletter Scraping Vorgang für eine Objekt-Seite
 url = urls[0]
-url='https://www.immowelt.de/expose/2xcl64z'
+url
+#url='https://www.immowelt.de/expose/2xcl64z'
 
 res = request.request("GET", url)
 parser = etree.HTMLParser(recover=True, encoding="utf-8")
 tree = etree.HTML(res.data, parser)
 
 # hiermit wird die Test-Seite gedownloadet.
-#with open("beispiel_haus_mieten.txt",'a') as f:
-#    f.write(res.data.decode('utf-8'))
+with open("beispiel_haus_kaufen.html",'a') as f:
+    f.write(res.data.decode('utf-8'))
 
 id = get_id_from_url(url)
 data = {}
@@ -102,22 +60,33 @@ for key, xpath_pattern in xpath_patterns.items():
     data[id][key]  = uncleanContent
 
 #-----manche xpath - Element sind Listen und man benötigt nur wenige Elemente
-for key in ['title','ort','merkmale','anzahl_raeume']:
+for key in ['title','ort','merkmale','anzahl_raeume','preis']:
     data[id][key] = data[id][key][0]
 
 data[id]['wohnflaeche'] = data[id]['wohnflaeche'][3]
 data[id]['grundstuecksflaeche'] = data[id]['grundstuecksflaeche'][5]
 
-data
+[key for key in data[id].keys()]
 
+for key, value in data[id].items():
+    try:
+        data[id][key] = clean_whitespace(data[id][key] )
+    except:
+        continue
+
+data
 #---- die Energiefelder werden direkt ausgelesen und das geht am einfachsten über eine for loop.
 energieListe=["Energieausweistyp","Baujahr laut Energieausweis","Wesentliche Energieträger","Endenergieverbrauch","Energieeffizienzklasse","Gültigkeit"]#,"Wesentliche EnergietrÃ¤ger","Endenergieverbrauch'']
 for item in energieListe:
     # vollständige liste
     # tree.xpath('//div[@class="datarow clear"]//span//text()')
-    #print(item)
-    path = '//div[@class="datarow clear" and contains(string(),"'+item+'")]//span//text()'
-    data[id][item]= str(tree.xpath(path)[1])
+
+    try:
+        path = '//div[@class="datarow clear" and contains(string(),"'+item+'")]//span//text()'
+        data[id][item]= str(tree.xpath(path)[1])
+        print(item)
+    except:
+        continue
 
 # ------- Bereinigung der Felder um \r\n und whitespaces
 # es gibt kurze, nur ein text-Element umfassende zu reinigende Elemente
