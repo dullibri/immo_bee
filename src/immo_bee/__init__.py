e30fc0bf85fa3e60c979240a0b5a68b65430db1a
Dirk Ulbricht <dirk.ulbricht@gmail.com>
2021-08-29 18:04:43 +0200

    bee (scraping) is usable also after import

--- src/immo_bee/__init__.py
+++ src/immo_bee/__init__.py
@@ -1,3 +1,5 @@
+from numpy import busday_count
+
 from . import getargs as getarg
 from . import scraping as scrap
 from .cleaning import *
@@ -11,18 +13,40 @@ def process_url(url):
     print("Scraping completed")
 
 
-def bee(rent=True, buy=True, house=True, appartment=True, locations=None):
+def bee(locations=None, rent=True, buy=True, house=True, appartment=True):
+    """Scrapes locations from immowelt.de and returns a dataframe of the
+    preprocessed data. Default: all houses and appartments and all offerings
+    for rent and sale are included.
 
+    If only house/appartment or rent/buy are
+    specified the other option is excluded. For example, if house is set to
+    True, appartment is set to False. If rent is set to True, buy is set to
+    False and vice versa.
+
+    Args:
+        locations (list of strings, optional): . Defaults to None.
+        rent (bool, optional): include housing for rent. Defaults to True.
+        buy (bool, optional): include housing for sale. Defaults to True.
+        house (bool, optional): include houses. Defaults to True.
+        appartment (bool, optional): include appartments. Defaults to True.
+
+    Returns:
+        pandas.DataFrame: All offerings preprocessed.
+    """
+    print(locations)
     if locations:
 
         class inputs:
-            rent = rent
-            buy = buy
-            house = house
-            appartment = appartment
-            locations = locations
+            pass
 
-        arguments = inputs
+        arguments = inputs()
+        arguments.locations = list()
+        arguments.locations.append(locations)
+        arguments.rent = rent
+        arguments.buy = buy
+        arguments.house = house
+        arguments.appartment = appartment
+
     else:
         arguments = getarg.get_arguments()
 
@@ -30,6 +54,7 @@ def bee(rent=True, buy=True, house=True, appartment=True, locations=None):
     for url in start_urls:
         process_url(url)
     df = load_and_prepare_data()
+
     if locations:
         return df
     else:
